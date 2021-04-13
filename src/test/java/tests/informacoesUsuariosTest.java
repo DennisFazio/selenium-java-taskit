32e70e2e3180c5a83922c100bf3aec661c0f8b0c
Dennis Fazio <fazioferreira@gmail.com>
2021-04-13 06:14:07 -0300

    Primeira movimentação

--- src/test/java/tests/informacoesUsuariosTest.java
+++ src/test/java/tests/informacoesUsuariosTest.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class informacoesUsuariosTest {
 
     @Test
@@ -17,17 +19,17 @@ public class informacoesUsuariosTest {
         System.setProperty("webdriver.chrome.driver","C:\\Windows\\System32\\chromedriver.exe" );
         WebDriver navegador = new ChromeDriver();
         navegador.manage().window().maximize();
+        navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
         //Abrindo a pagina
         navegador.get("http://www.juliodelima.com.br/taskit/");
-        assertEquals(1,1);
 
         // Mapeando os elementos
         WebElement btnSignIn = navegador.findElement(By.linkText("Sign in"));
         WebElement boxSigIn = navegador.findElement(By.id("signinbox"));
         WebElement boxSigInInputUser = boxSigIn.findElement(By.name("login"));
         WebElement boxSigInInputPassword = boxSigIn.findElement(By.name("password"));
-        WebElement boxSigInBtnSignIn = navegador.findElement(By.linkText("SIGN IN"));
+        WebElement boxSigInBtnSignIn = boxSigIn.findElement(By.cssSelector("a[class=\"modal-action waves-effect waves-green btn-flat\"]"));
 
 
         //Clicar no link do texto "Sign In"
@@ -43,9 +45,12 @@ public class informacoesUsuariosTest {
         boxSigInBtnSignIn.click();
 
         // Validar se o campo class "me" possui o texto "Hi, Dedera Fazio"
+        WebElement me = navegador.findElement(By.className("me"));
+        String textoNoElementoME = me.getText();
+        assertEquals("Hi, Dedera Fazio", textoNoElementoME);
 
         //Fechar Navegador
-        navegador.close();
+        navegador.quit();
 
     }
 
